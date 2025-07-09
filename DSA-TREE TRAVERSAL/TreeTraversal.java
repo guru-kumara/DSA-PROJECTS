c83e0a1e7e6c7877a298bac7317882482ff210c7
kumaraguru <146850289+guru-kumara@users.noreply.github.com>
2025-07-10 00:16:03 +0530

    Add files via upload

---
+++ DSA-TREE TRAVERSAL/TreeTraversal.java
@@ -0,0 +1,51 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+public class TreeTraversal {
+    public void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.val + " ");
+            inorder(root.right);
+        }
+    }
+
+    public void preorder(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public void postorder(TreeNode root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.val + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        TreeTraversal traversal = new TreeTraversal();
+        System.out.print("Inorder: ");
+        traversal.inorder(root);
+        System.out.print("\nPreorder: ");
+        traversal.preorder(root);
+        System.out.print("\nPostorder: ");
+        traversal.postorder(root);
+    }
+}
